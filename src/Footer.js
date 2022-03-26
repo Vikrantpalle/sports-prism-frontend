842e2288b92fe0d36b63d6751db208ee7a985e4b
GAURCODER <96721318+GAURCODER@users.noreply.github.com>
2022-03-27 02:22:33 +0530

    modificatins for the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,43 +1,28 @@
-import React from 'react'
-import './Footer.css'
-import {Container,Row,Col} from 'react-bootstrap'
-class Footer extends React.Component{
-    render(){
-        return(
-            <Container fluid className='footer mt-4'>
-
-        <Row className="justify-content-md-center socials mb-3" xs="auto">
-           <Col>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-twitter" width="34" height="34" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"></path>
-             </svg>
-             </Col>
-      <Col>
-             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width="34" height="34" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
-             </svg>
-             </Col>
-             <Col>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-telegram" width="34" height="34" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
-                 </svg>              
-                 </Col>
-        </Row>
-
-        <Container fluid>
-            <Row className='justify-content-md-center mb-3'>
-            
+import React from "react";
+import "./Footer.css";
+import { Row } from "react-bootstrap";
+class Footer extends React.Component {
+  render() {
+    return (
+      <div class="footer">
+        <div class="social-icon">
+          <a href="#" class="fa fa-linkedin fa-2x social"></a>
+          <a href="#" class="fa fa-instagram fa-2x social" target="_blank"></a>
+          <a href="#" class="fa fa-github fa-2x social"></a>
+          <a
+            href="#"
+            class="fa fa-facebook-square fa-2x social"
+            aria-hidden="true"
+          ></a>
+        </div>
+        <div class="fluid">
+          <Row className="justify-content-center mb-3 mt-5">
             Copyright © 2022 SPORTSPRISM
-       
-            </Row>
-            </Container>
-
-    </Container>
-        );
-    }
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
